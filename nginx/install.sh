3c86e2caf9f9ff63cd1331fa6997d36d8b4f6c23
root <fred>
2019-04-19 15:46:52 +0800

    add a file for the nginx env

--- nginx/install.sh
+++ nginx/install.sh
@@ -55,6 +55,7 @@ yes | cp index.html.default /data/wwwroot/default/index.html
 yes | cp nginx.conf /usr/local/nginx/conf/nginx.conf
 yes | cp nginx.logrotate /etc/logrotate.d/nginx
 yes | cp nginx.service /usr/lib/systemd/system/nginx.service
+yes | cp nginx-env.sh /etc/profile.d/nginx-env.sh
 
 systemctl daemon-reload && systemctl restart nginx && systemctl enable nginx
 if [ $? -ne 0 ];then
---
+++ nginx/nginx-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export NGX_HOME=/usr/local/nginx
+export PATH=$NGX_HOME/sbin:$PATH
